b547269190ed1b337c0a7448f545be7391b7d5c9
ArseneMerci <harsenemerci@gmail.com>
2022-07-14 16:20:37 +0200

    setting up

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Navbar, Nav, Container} from 'react-bootstrap';
+import Home from './Home';
+import About from './About';
+import Users from './Users';
+import { Router, Route, Link, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home"> <Link to="/">Home</Link> </Nav.Link>
+            <Nav.Link href="#features"><Link to="/about">About</Link> </Nav.Link>
+            <Nav.Link href="#pricing"><Link to="/users">Users</Link> </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/users" element={<Users/>} />
+    </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/Users.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Users(props) {
+    return (
+        <div>
+            <h1>User's component</h1>
+        </div>
+    );
+}
+
+export default Users;
