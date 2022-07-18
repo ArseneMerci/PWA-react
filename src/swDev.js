88fc30ac592cf689c86c00b5eb3157c37ae403c0
ArseneMerci <harsenemerci@gmail.com>
2022-07-18 09:57:46 +0200

    added configuration for push notification

--- src/swDev.js
+++ src/swDev.js
@@ -1,6 +1,32 @@
 export default function swDev(){
+    function determineAppServerKey(){
+        const vapidPublicKey = 'BF7tZWoV2kRken-cDhmqoC0O1QjtwcyxpNXttdS7AFXX3PvTX5PJBVlxc1QbYKCEfeVJOmz1qnU1go4dtizrACA';
+        return urlBase64ToUint8Array(vapidPublicKey);
+    }
+    function urlBase64ToUint8Array(base64String) {
+        const padding = '='.repeat((4 - base64String.length % 4) % 4);
+        const base64 = (base64String + padding)
+          .replace(/-/g, '+')
+          .replace(/_/g, '/');
+      
+        const rawData = window.atob(base64);
+        const outputArray = new Uint8Array(rawData.length);
+      
+        for (let i = 0; i < rawData.length; ++i) {
+          outputArray[i] = rawData.charCodeAt(i);
+        }
+        return outputArray;
+      }
+
     let swUrl= `${process.env.PUBLIC_URL}/sw.js`;
     navigator.serviceWorker.register(swUrl).then((response)=>{
         console.log("response",response);
+        response.pushManager.getSubscription()
+            .then((subscription)=>{
+                return response.pushManager.subscribe({
+                    userVisibleOnly: true,
+                    applicationServerKey: determineAppServerKey()
+                });
+            })
     })
 }
